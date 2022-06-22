5b2c4093760a33e55b29192ecab99b5ea798c3d0
knotted48 <99110172+knotted48@users.noreply.github.com>
2022-06-22 11:39:37 +0900

    Create 11651.c

---
+++ 0622/11651.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Coordinate {
+    int x;
+    int y;
+} xy;
+
+void Swap(xy *a, xy *b) {
+    xy tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
+void Heapify(xy arr[], int n, int i) {
+    int root = i;
+    int left = root * 2 + 1;
+    int right = root * 2 + 2;
+    
+    if (left < n && arr[root].y < arr[left].y)
+        root = left;
+    else if (left < n && arr[root].y == arr[left].y && arr[root].x < arr[left].x)
+        root = left;
+    
+    if (right < n && arr[root].y < arr[right].y)
+        root = right;
+    else if (right < n && arr[root].y == arr[right].y && arr[root].x < arr[right].x)
+        root = right;
+    
+    if (root != i) {
+        Swap(&arr[i], &arr[root]);
+        Heapify(arr, n, root);
+    }
+}
+
+void HeapSort(xy arr[], int n) {
+    for (int i = n / 2 - 1; i >= 0; i--)
+        Heapify(arr, n, i);
+    
+    for (int i = n - 1; i >= 0; i--) {
+        Swap(&arr[0], &arr[i]);
+        Heapify(arr, i, 0);
+    }
+}
+
+int main(void) {
+    int n;
+    xy *pair;
+    
+    scanf("%d", &n);
+    
+    pair = (xy*)malloc(sizeof(xy) * n);
+    for (int i = 0; i < n; i++)
+        scanf("%d%d", &pair[i].x, &pair[i].y);
+    
+    HeapSort(pair, n);
+    
+    for (int i = 0; i < n; i++)
+        printf("%d %d\n", pair[i].x, pair[i].y);
+    
+    return 0;
+}
